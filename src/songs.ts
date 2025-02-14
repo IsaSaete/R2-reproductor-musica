f7e7c363e2d5e68801dd4f5df63d937769d88343
Isabel Sáenz <isasaete@gmail.comn>
2025-02-14 19:26:23 +0100

    Created the typing of the song

--- src/songs.ts
+++ src/songs.ts
@@ -2,7 +2,10 @@ import { Song } from "./songsData";
 
 export const areSameSong = (song: Song, songTitle: string): boolean => {
   let areSame: boolean;
-  areSame = song.title === songTitle;
+  // Asígnale a la variable areSame un true o un false dependiendo de si
+  // el título de la canción recibida coincide con el título recibido
+
+  areSame = true; // Cambia esta línea por tu código
 
   return areSame;
 };
